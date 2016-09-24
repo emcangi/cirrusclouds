c277c80da3f7bade8b0260423e39400f43e81c59
Eryn Cangi <emcangi@gmail.com>
2016-09-23 17:22:04 -0700

    Fixed polyphot_batch so that it works with images in any folder, and places the photometry files in the folders with their parent images. Fixed tidy_stats to properly print the image file path for the CORRECT image, and to do it without the filename appended. Also added a text document showing the procedure for using the scripts.

--- polyphot_batch.py
+++ polyphot_batch.py
@@ -6,41 +6,49 @@
 # 1 September 2016
 # ============================================================================ #
 
-from pyraf import iraf
 
 def prepare_params():
     """
     Grabs the parameters for each image out of a storage file and puts them
     into a list. Also generates a list of logfile names.
 
-    Format of storage file is:
-    IMAGENAME    SKYVALUE    SIGMA    EXPOSURE    FULLPATH (user dependent)
+    Format of storage file must be:
+    IMAGENAME    SKYVALUE    SIGMA    EXPOSURE    PATH
+
+    The 'PATH' is the path to the image file, not including the image file
+    itself, and will be user- or computer- dependent.
     """
 
-    choice = raw_input('Enter option to select image & parameter file '
-                           'path.\n'
-                           '[1]: Enter full path to the file\n'
-                           '[2]: Enter just the working directory name (use '
-                           'the default base path)\n\n')
+    default = '/home/emc/GoogleDrive/Phys/Research/BothunLab' \
+                     '/AnalysisFiles/{}/files_and_params.txt'
+
+    print('Current default path to the image parameter file is {}, where curly '
+          'braces represent the working directory\n'.format(default))
+
+    choice = raw_input('How do you want to identify the image & parameter '
+                       'file?\n'
+                       '[1]: Enter full path to the file\n'
+                       '[2]: Enter just the working directory name (use '
+                       'the default path)\n'
+                       'Your choice: ')
 
     if choice == '1':
         param_file = raw_input('Please input path: ')
     elif choice == '2':
         d = raw_input('Please input directory name with spaces, no slashes: ')
-        param_file = '/home/emc/GoogleDrive/Phys/Research/BothunLab' \
-                     '/AnalysisFiles/{}/files_and_params.txt'.format(d)
+        param_file = default.format(d)
     else:
         while choice != 1 or choice != 2:
             choice = raw_input('Please enter either 1 or 2:\n'
                                '[1]: Enter full path to the file\n'
                                '[2]: Enter just the working directory name ('
-                               'use the default base path)\n\n')
-
+                               'use the default path)\n\n')
 
     # Read in the param_file and put values in lists
     image_data = []
 
     with open(param_file, 'r') as f:
+        throwaway = f.readline() # skip the first line which is a comment
         for line in f:
             ln = line.split()
             if ln:
@@ -52,31 +60,61 @@ def prepare_params():
     return image_data, logs
 
 
-def do_photometry(coordfile, polygonfile):
+def do_photometry():
     """
     Do photometry in batch mode.
-    :param coordfile: a text file of grid centers, as required by polyphot
-    :param polygonfile: a text file of polygon sizes, as required by polyphot
+    Script will prompt for the locations of the grid files and the location
+    of the parameter file.
 
     Output:
-    Files for each image with extension '_photometry' and data inside.
+    Files for each image with extension '_photometry' and data inside,
+    which are placed in the same directory as their parent image.
     """
 
-    # This just retrieves all parameters for all images from a stored file
-    # that I made
+    from pyraf import iraf
+    from os import walk
+
+    # Retrieves image parameters from a stored file ----------------------------
     image_data, lognames = prepare_params()
 
-    # Loops through images and calls polyphot for each. REQUIRED:
-    # Manually set interactive mode = no, validate = no in epar polyphot at
-    # pyraf prompt.
+    # Identify the grid files --------------------------------------------------
+    default = '/home/emc/GoogleDrive/Phys/Research/BothunLab/AnalysisFiles' \
+              '/gridfiles/'
+
+    print('Default path for coordinate and polygon files: \n{}'.format(default))
+    use_default = raw_input('Use base path? (y/n): ')
+    if use_default == 'n':
+        path = raw_input('Enter new path to coordinate and polygon files: ')
+    elif use_default == 'y':
+        path = default
+    else:
+        while use_default != 'n' or use_default != 'y':
+            use_default = raw_input('Please enter y or n: ')
+
+    print('Available grid files: ')
+
+    for (dirpath, dirnames, gfile) in walk(path):
+        print(gfile)
+
+    gsize = raw_input('Enter the grid size to use (ex: 10x10): ')
+
+    coordfile = gsize + 'grid_centers.txt'
+    polygonfile = gsize + 'grid_polygons.txt'
+
+    # Loops through images and call polyphot for each --------------------------
     for image, logname in zip(image_data, lognames):
         filename = image[0]
         sky = image[1]
         sig = image[2]
         exp = image[3]
+        im_path = image[4]
 
         # call the task
-        iraf.polyphot(filename, coords=coordfile, output=logname,
-                      polygons=polygonfile, skyvalue=sky, sigma=sig, itime=exp)
+        iraf.polyphot(im_path+filename, coords=path+coordfile,
+                      output=im_path+logname, polygons=path+polygonfile,
+                      interactive='no', skyvalue=sky, sigma=sig, itime=exp,
+                      verify='no')
+
     print('Photometry complete!')
+
     return None
