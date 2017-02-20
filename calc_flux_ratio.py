48b92aa40cd16c38b7151893edd8b7ef999523ef
Eryn Cangi <emcangi@gmail.com>
2017-02-19 23:20:16 -0800

    Better names for variables reflecting the fact that the flux ratios calculated are usually for B-V values. Inclusion of code for plotting results. Plotting code will be moved to its own file next time.

--- calc_flux_ratio.py
+++ calc_flux_ratio.py
@@ -14,6 +14,7 @@ from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
+import math
 
 
 def make_dataframe(phot_file, img_file):
@@ -30,17 +31,10 @@ def make_dataframe(phot_file, img_file):
 
     # Copy polyphot data to new file without headers --------------------------
     out_file = phot_file + '_data'
-    print('I am using {}'.format(out_file))
 
     # TODO: fix this so the number of lines is determined from file, not a const
     lines = 83
     os.system("tail -n +{} {} > {}".format(lines, phot_file, out_file))
-
-    # Get FITS data and save for using in matplotlib ===========================
-    # hdu_list = fits.open(img_file)
-    # hdu_list.info()
-    # image_data = hdu_list[0].data
-    # hdu_list.close()
 
     # Construct initial lists of information from the photometry file ==========
     ln_cnt = 1  # we have to keep track since Python doesn't
@@ -103,11 +97,9 @@ def make_dataframe(phot_file, img_file):
         big_table.append(datum)
 
     # Make a Pandas dataframe ==================================================
-    print(big_table[0:30])
     headers = big_table[0]
     df = DataFrame(big_table[1:], columns=headers)
-
-    return df, 'trash', img_metadata#image_data, img_metadata
+    return df, img_metadata
 
 
 # ==============================================================================
@@ -116,55 +108,128 @@ def make_dataframe(phot_file, img_file):
 default = '/home/emc/GoogleDrive/Phys/Research/BothunLab/SkyPhotos/NewCamera'
 print('Current default base path is {}\n'.format(default))
 
-dir_ext1 = raw_input('Please enter the directory(ies) housing the first image '
+dir_extV = raw_input('Please enter the directory(ies) housing the first image '
                      'and photometry file (e.g. '
                      '28October2016/15-11/260microsec): ')
-dir_ext2 = raw_input('Please enter the directory(ies) housing the second image '
+dir_extB = raw_input('Please enter the directory(ies) housing the second image '
                      'and photometry file (e.g. '
                      '28October2016/15-11/260microsec): ')
 
-img1 = raw_input('First image file name: ')
-img2 = raw_input('Second image file name: ')
+imgV = raw_input('First image file name: ')
+imgB = raw_input('Second image file name: ')
 size = '64x64'#raw_input('Please input the photometry grid size (e.g. 64x64): ')
-path1 = '/'.join([default, dir_ext1])
-path2 = '/'.join([default, dir_ext2])
-path1 += '/'
-path2 += '/'
+pathV = '/'.join([default, dir_extV])
+pathB = '/'.join([default, dir_extB])
+pathV += '/'
+pathB += '/'
 
 # Identify the image files and associated photometry files
-img_file1 = path1+img1 if img1[-4:] == '.FIT' else path1+img1+'.FIT'
-img_file2 = path2+img2 if img2[-4:] == '.FIT' else path2+img2+'.FIT'
-phot_file1 = img_file1[:-4] + '_photometry_' + size
-phot_file2 = img_file2[:-4] + '_photometry_' + size
+img_fileV = pathV + imgV if imgV[-4:] == '.FIT' else pathV + imgV + '.FIT'
+img_fileB = pathB + imgB if imgB[-4:] == '.FIT' else pathB + imgB + '.FIT'
+phot_fileV = img_fileV[:-4] + '_photometry_' + size
+phot_fileB = img_fileB[:-4] + '_photometry_' + size
 
-print('Comparing images: {} \n and \n (#2)'.format(img_file1, img_file2))
-print('Using photometry files: {} \n and \n {}'.format(phot_file1, phot_file2))
+print('Comparing images: {} \n and \n {}(#2)'.format(img_fileV, img_fileB))
+#print('Using photometry files: {} \n and \n {}'.format(phot_fileV, phot_fileB))
 
-df1, img_data1, img_metadata1 = make_dataframe(phot_file1, img_file1)
-df2, img_data2, img_metadata2 = make_dataframe(phot_file2, img_file2)
+dfV, img_metadataV = make_dataframe(phot_fileV, img_fileV)
+dfB, img_metadataB = make_dataframe(phot_fileB, img_fileB)
 
-print(df1['Flux'][0:30])
-print(img_metadata1)
-print(df2['Flux'][0:1000])
-print(img_metadata2)
 # ==============================================================================
 # CALCULATE THE FLUX RATIOS
 # ==============================================================================
 
 # Calculate flux ratio of image #1 to #2 (#1 is in numerator). Using vertices
 #  from either dataframe since they are the same
-f_ratio_1to2_df = DataFrame({'F Ratio': np.array((df1['Flux'] / df2['Flux']),
-                             dtype='float32'),
-                                'v1': df1['v1'],
-                                'v2': df1['v2'],
-                                'v3': df1['v3'],
-                                'v4': df1['v4']})
+print('Flux ratio F_v/F_b:')
+FRatio_VtoB_df = DataFrame({'F Ratio': np.array((dfV['Flux'] / dfB['Flux']),
+                                                dtype='float32'), 'v1': dfV['v1'],
+                                'v2': dfV['v2'], 'v3': dfV['v3'],
+                                'v4': dfV['v4']})
 
-# Calculate the inverse, #2 to #1
-f_ratio_2to1_df = DataFrame(1/f_ratio_1to2_df['F Ratio'])
+B_V_df = DataFrame({'B-V': -2.5 * np.log10(np.array((dfV['Flux'] / dfB['Flux']),
+                    dtype='float32')), 'v1': dfV['v1'], 'v2': dfV['v2'],
+                    'v3': dfV['v3'], 'v4': dfV['v4']})
 
-print(f_ratio_1to2_df[0:30])
+
+print(B_V_df[0:13])
 print()
-print(f_ratio_2to1_df[0:30])
+
+print(B_V_df.iloc[4]['B-V'])
+print(math.isnan(B_V_df.iloc[4]['B-V']))
+
+# ==============================================================================
+# PLOT STUFF (may move this later)
+# ==============================================================================
+
+# Get FITS data and save for using in matplotlib ===========================
+dir_extN = raw_input('Please enter the directory(ies) housing the image taken '
+                     'with no filter (ex. 28October2016/None/260microsec): ')
+imgN = raw_input('No-filter image file name: ')
+pathN = '/'.join([default, dir_extN, imgN])
+img_fileN = pathN if pathN[-4:] == '.FIT' else pathN + '.FIT'
+hdu_list = fits.open(img_fileN)
+hdu_list.info()
+image_data = hdu_list[0].data
+hdu_list.close()
+
+# create plot with image as background
+fig1 = plt.figure(figsize=(16, 12))
+ax1 = fig1.add_subplot(111)
+plt.imshow(image_data, cmap='gray')
+ax1.set_autoscale_on(False)
+
+# Draw boxes on the image where there is a valid B-V value
+for index, row in B_V_df.iterrows():  # iterate over the rows
+    if math.isnan(row['B-V']):
+        continue
+    else:
+        # collect the vertices for the boxes
+        r1 = row['v1']
+        r2 = row['v2']
+        r3 = row['v3']
+        r4 = row['v4']
+
+        # including the 1st point at the end again allows the boxes to be closed
+        x = np.array([r1[0], r2[0], r3[0], r4[0], r1[0]])
+        y = np.array([r1[1], r2[1], r3[1], r4[1], r1[1]])
+
+        # ax.scatter(x, y, c='lime') # toggle to turn on plotting vertices
+        ax1.plot(x, y, c='lime')
+        ax1.set_title('{}'.format(imgN), fontsize=20)
+        ax1.get_xaxis().set_visible(False)
+        ax1.get_yaxis().set_visible(False)
+
+plt.show()
+
+# create plot with image as background
+fig2 = plt.figure(figsize=(16, 12))
+ax2 = fig2.add_subplot(111)
+plt.imshow(image_data, cmap='gray')
+ax2.set_autoscale_on(False)
+
+# Draw boxes on the image where B-V value is < 0, i.e. rather blue
+for index, row in B_V_df.iterrows():  # iterate over the rows
+    if row['B-V'] > 0:
+        continue
+    elif row['B-V'] < 0:
+        # collect the vertices for the boxes
+        r1 = row['v1']
+        r2 = row['v2']
+        r3 = row['v3']
+        r4 = row['v4']
+
+        # including the 1st point at the end again allows the boxes to be closed
+        x = np.array([r1[0], r2[0], r3[0], r4[0], r1[0]])
+        y = np.array([r1[1], r2[1], r3[1], r4[1], r1[1]])
+
+        # ax.scatter(x, y, c='lime') # toggle to turn on plotting vertices
+        ax2.plot(x, y, c='lime')
+        ax2.set_title('{}'.format(imgN), fontsize=20)
+        ax2.get_xaxis().set_visible(False)
+        ax2.get_yaxis().set_visible(False)
+    else:
+        continue
 
 
+plt.show()
