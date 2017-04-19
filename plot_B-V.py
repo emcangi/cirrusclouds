ade142dc44aea5d18240d514418fbcf8772bfb7b
Eryn Cangi <emcangi@gmail.com>
2017-04-19 10:52:42 -0700

    Updated plotting script so that it properly creates, titles, and saves plots into a proper subdirectory of the given data set. Still to do: Update script so that it takes input on which things to plot from a file and make it run in batch over those inputs.

--- plot_B-V.py
+++ plot_B-V.py
@@ -16,84 +16,86 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from ast import literal_eval
 import numpy as np
+import re
+import math
+import os
 
-# Get FITS data and save for using in matplotlib ===========================
+# COLLECT INFORMATION ON IMAGES TO USE =========================================
 default = '/home/emc/GoogleDrive/Phys/Research/BothunLab/SkyPhotos/NewCamera'
 dir_extN = raw_input('Please enter the directory(ies) housing the image taken '
                      'with no filter (ex. 28October2016/None/260microsec): ')
 imgN = raw_input('No-filter image file name: ')
 pathN = '/'.join([default, dir_extN, imgN])
 img_fileN = pathN if pathN[-4:] == '.FIT' else pathN + '.FIT'
+
+# OPEN FLUX RATIO DATAFRAME ====================================================
+dffile = raw_input('Please input the full path of the CSV file containing '
+                   'dataframe info: ')
+csvonly = re.search(r'FR.+.csv$', dffile).group(0)   # collects csv file name
+setpath = re.search(r'.+(?=FR.+.csv$)', dffile).group(0)  # where to save plots
+B_V_df = pd.read_csv(dffile)
+
+# find min, max B-V values -----------------------------------------------------
+good = B_V_df.loc[B_V_df['B-V'] != -9999]
+max_bv = math.ceil(good.max(numeric_only=True)['B-V'])
+min_bv = math.floor(good.min(numeric_only=True)['B-V'])
+
+# CREATE PLOTS =================================================================
+
+# Load the FITS data for the no-filter image -----------------------------------
 hdu_list = fits.open(img_fileN)
 hdu_list.info()
 image_data = hdu_list[0].data
 hdu_list.close()
 
-# Gather the dataframe
-dffile = raw_input('Please input the full path of the CSV file containing '
-                   'dataframe info: ')
-B_V_df = pd.read_csv(dffile)
+# set max, min of B-V and the window size for plotting -------------------------
+resolution = 0.5
+cutoffs = np.arange(min_bv, max_bv, resolution)
 
-# Draw boxes on the image where there is a valid B-V value
+# collect filter names from CSV file name --------------------------------------
+csvonly = re.sub('FR_B-V_', '', csvonly)
+csvonly = re.sub('.csv', '', csvonly)
+filter1 = re.search(r'.+(?=-)', csvonly).group(0)
+filter2 = re.search(r'(?<=-).+', csvonly).group(0)
 
-# create plot with image as background
-fig1 = plt.figure(figsize=(16, 12))
-ax1 = fig1.add_subplot(111)
-plt.imshow(image_data, cmap='gray')
-ax1.set_autoscale_on(False)
+# create a directory for the plots to live -------------------------------------
+imgpath = setpath + 'plots/' + '{}-{}/'.format(filter1, filter2)
+if not os.path.exists(imgpath):
+    os.makedirs(imgpath)
 
-# iterate over the rows
-for index, row in B_V_df.iterrows():
-    if row['B-V'] == -9999:           # ignore invalid B-V entries
-        continue
-    else:
-        # collect the vertices for the boxes
-        r1 = literal_eval(row['v1'])
-        r2 = literal_eval(row['v2'])
-        r3 = literal_eval(row['v3'])
-        r4 = literal_eval(row['v4'])
+for i in range(len(cutoffs) - 1):
+    # Create figure, plot no-filter FITS image as background -------------------
+    fig = plt.figure(figsize=(16, 12))
+    ax = fig.add_subplot(111)
+    plt.imshow(image_data, cmap='gray')
+    ax.set_autoscale_on(False)
 
-        # including the 1st point at the end again allows the boxes to be closed
-        x = np.array([r1[0], r2[0], r3[0], r4[0], r1[0]])
-        y = np.array([r1[1], r2[1], r3[1], r4[1], r1[1]])
+    # iterate over the rows in the dataframe, plotting boxes -------------------
+    color = np.random.rand(3, 1)
+    for index, row in good.iterrows():
+        if cutoffs[i] < row['B-V'] < cutoffs[i+1]:
+            # collect the vertices for the boxes
+            r1 = literal_eval(row['v1'])
+            r2 = literal_eval(row['v2'])
+            r3 = literal_eval(row['v3'])
+            r4 = literal_eval(row['v4'])
 
-        # ax.scatter(x, y, c='lime') # toggle to turn on plotting vertices
-        ax1.plot(x, y, c='lime')
-        # print('Plotted a vertex ({})'.format(n))
-        # n += 1
+            # Create the boxes
+            #  including the 1st point at the end allows the boxes to be closed
+            x = np.array([r1[0], r2[0], r3[0], r4[0], r1[0]])
+            y = np.array([r1[1], r2[1], r3[1], r4[1], r1[1]])
 
-ax1.set_title('Valid B-V values for {}'.format(imgN), fontsize=20)
-ax1.get_xaxis().set_visible(False)
-ax1.get_yaxis().set_visible(False)
-plt.show()
+            # ax.scatter(x, y, c='lime')  # toggle to turn on plotting vertices
+            ax.plot(x, y, c=color)
 
-# create plot with image as background
-fig2 = plt.figure(figsize=(16, 12))
-ax2 = fig2.add_subplot(111)
-plt.imshow(image_data, cmap='gray')
-ax2.set_autoscale_on(False)
+    ax.set_title('B-V values between {} and {} for filters {} (B) and {} ('
+                 'V)'.format(cutoffs[i], cutoffs[i+1], filter1, filter2),
+                 fontsize=20)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
-# Draw boxes on the image where B-V value is < 0, i.e. rather blue
-for index, row in B_V_df.iterrows():  # iterate over the rows
-    if row['B-V'] > 0:
-        continue
-    elif row['B-V'] < 0:
-        # collect the vertices for the boxes
-        r1 = literal_eval(row['v1'])
-        r2 = literal_eval(row['v2'])
-        r3 = literal_eval(row['v3'])
-        r4 = literal_eval(row['v4'])
-
-        # including the 1st point at the end again allows the boxes to be closed
-        x = np.array([r1[0], r2[0], r3[0], r4[0], r1[0]])
-        y = np.array([r1[1], r2[1], r3[1], r4[1], r1[1]])
-
-        # ax.scatter(x, y, c='lime') # toggle to turn on plotting vertices
-        ax2.plot(x, y, c='lime')
-    else:
-        continue
-
-ax2.set_title('B-V < 0 (blue) {}'.format(imgN), fontsize=20)
-ax2.get_xaxis().set_visible(False)
-ax2.get_yaxis().set_visible(False)
-plt.show()
+    # save figure --------------------------------------------------------------
+    fname = 'B-V_{}to{}_filters_{}_and_{}.png'.format(cutoffs[i], cutoffs[i+1],
+                                                      filter1, filter2)
+    plt.savefig(imgpath+fname, bbox_inches='tight')
+    plt.close()
