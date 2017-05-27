62b781d853b15d46978fda16f090892f26928631
Eryn Cangi <emcangi@gmail.com>
2017-05-27 10:55:55 -0700

    Naturally the previous version was idiotic and didn't work if there was more than one image per exposure per filter. Fixed it in this version, which allows any number of photos per filter, any arrangement of filters. Sorts the commands by filter order 11, 15, 47, 82a, LRGBred, LRGBgreen, LRGBblue, LRGBluminance.

--- generate_manual_cmds.py
+++ generate_manual_cmds.py
@@ -7,74 +7,66 @@
 # Use any way you like. Can easily be run as-is in Sublime.
 # ============================================================================ #
 
-# Change these as needed
 thepath = '/home/emc/GoogleDrive/Phys/Research/BothunLab/SkyPhotos/NewCamera/' \
          '{}'
 setkey = '/{}/'   # key to use to find the right set of stuff
-exposurekey = ''  # only fill in if there is more than one exposure
+expkey = ''  # only fill in if there is more than one exposure
 
+# Get input from user
 dated_folder = raw_input('Enter the dated image folder: ')
-theset = raw_input('Enter the name of the set folder to generate commnads '
+set_input = raw_input('Enter the name of the set folder to generate commnads '
                    'for: ')
-expkey = raw_input('Enter the exposure to look for (press enter if none): ')
+e_input = raw_input('Enter the exposure to look for (press enter if none): ')
 
+# set up some more variables we will need
 thefile = thepath.format(dated_folder) + '/files_and_params.txt'
-setkey = setkey.format(theset)
-exposurekey = exposurekey.format(expkey)
+setkey = setkey.format(set_input)
+expkey = expkey.format(e_input)
 
+# this chops off the _# extension to a folder, where _# is my system for
+# prioritizing data sets.
 if '_' in dated_folder:
     date = dated_folder.split('_')[0]
+else:
+    date = dated_folder
 
-# do not change the below
+# for building the commands and lists of commands
 BASE = 'polyphot {} coords="" polygons="" output="{}" sigma={} itime={} ' \
        'ifilter={} skyvalue={} interactive=yes'
-CMDS_UNSORT = []  # list of commands, used to sort before printing
-cmds_sort = ['']*8
+cmds_unsort = []  # list of commands, used to sort before printing
+cmds_sort = []
 
+# Loop through lines in the file and extract necessary info, add commands to
+# unsorted list
 with open(thefile, 'r') as f:
     junk = f.readline()
     for line in f:
         data = line.split()
         if data:
-            if setkey in data[7] and exposurekey in data[7]:
+            if setkey in data[7] and expkey in data[7]:
                 fn = data[0]
                 outputfn = fn[:-4] + '_photometry_manual'
                 sig = data[2]
                 sky = data[1]
                 e = data[4]
                 filt = data[5] + ',' + data[6]
-                CMDS_UNSORT.append(BASE.format(fn, outputfn, sig, e, filt, sky))
+                cmds_unsort.append(BASE.format(fn, outputfn, sig, e, filt, sky))
 
-# This is the ugliest shit I've ever written and it sure isn't Pythonic but
-# gosh darn it, it works.
+# this is used for sorting the commands
+possfilters = ['11', '15', '47', '82a', 'LRGBred', 'LRGBgreen',
+                       'LRGBblue', 'LRGBluminance', 'None']
 
-for cmd in CMDS_UNSORT:
-    if "ifilter=11" in cmd:
-        cmds_sort[0] = cmd
-    elif "ifilter=15" in cmd:
-        cmds_sort[1] = cmd
-    elif "ifilter=47" in cmd:
-        cmds_sort[2] = cmd
-    elif "ifilter=82a" in cmd:
-        cmds_sort[3] = cmd
-    elif "ifilter=LRGBred" in cmd:
-        cmds_sort[4] = cmd
-    elif "ifilter=LRGBgreen" in cmd:
-        cmds_sort[5] = cmd
-    elif "ifilter=LRGBblue" in cmd:
-        cmds_sort[6] = cmd
-    elif "ifilter=LRGBluminance" in cmd:
-        cmds_sort[7] = cmd
-    else:
-        pass
+# sort the commands based on the filter
+for fil in possfilters:
+    for c in cmds_unsort:
+        if 'ifilter={}'.format(fil) in c:
+            cmds_sort.append(c)
 
+# write out the sorted commands to the file
 fn = 'man_polyphot_cmds_{}_{}.txt'
-
 writepath = thepath+setkey+fn
-
-with open(writepath.format(dated_folder, date, theset), 'w') as f:
+import pickle
+with open(writepath.format(dated_folder, date, set_input), 'w') as f:
     for cmd in cmds_sort:
-        f.write(cmd + '\n')
-
-
+        f.write(cmd)
 
