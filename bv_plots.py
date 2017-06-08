540b8f4cdb13d7bcb5cbac7db844756e52643fa2
Eryn Cangi <emcangi@gmail.com>
2017-06-08 14:24:39 -0700

    Updated so the plot which displays both cloud and moon values with error bars is a scatter plot, not a bar plot

--- bv_plots.py
+++ bv_plots.py
@@ -105,15 +105,31 @@ def autolabelh(rects, vals):
 
 # collect path information to files
 basepath = '/home/{}/GoogleDrive/Phys/Research/BothunLab/SkyPhotos/NewCamera/'
-un = raw_input('Please specify username of this computer account: ')
+un = raw_input('Please specify username of this computer account (enter to '
+               'use default of "emc"): ')
+if un == '':
+    un = 'emc'
+else:
+    pass
 basepath = basepath.format(un)
+
 cloudpath = raw_input('Please enter the cloud set folder, '
                       'i.e. 3January2017/set01: ')
-comparedpath = raw_input('Please enter the comparison set folder, '
-                     'i.e. 10April2017_MOON/set01: ')
-
 full_cloud_path = basepath + cloudpath
-full_compared_path = basepath + comparedpath
+
+full_compared_path = ''
+while full_compared_path == '':
+    selectCompare = raw_input('Available comparison sets: \n '
+                              '[1] 10April2017_MOON/set01 \n'
+                              '[2] 26May2017_SUN/set01 \n '
+                              '\nSelect 1 or 2: ')
+    if selectCompare == '1':
+        comparedpath = '10April2017_MOON/set01'
+    elif selectCompare == '2':
+        comparedpath = '26May2017_SUN/set01'
+    else:
+        pass
+    full_compared_path = basepath + comparedpath
 
 # get the dates only--used for putting labels in the plots
 clouddate = re.search('([0-9]+[A-Za-z]+[0-9]+)', cloudpath).group(0)
@@ -166,7 +182,12 @@ if not os.path.exists(saveloc):
     os.makedirs(saveloc)
 
 # Plot difference of cloud B-V and moon B-V ------------------------------------
-# Determines whether to color a bar gold or blue depending on the value
+# Make the plot
+h = 0.8
+fig, ax = plt.subplots(figsize=(14, 10))
+
+# Determines whether to color a bar gold or blue depending on the value,
+# makes a legend
 colors = []
 for d in diffs:
     if abs(d) <= 0.05:
@@ -178,19 +199,18 @@ for d in diffs:
 
 gold = mpatches.Patch(color='gold', label='Diff <= 0.05')
 silver = mpatches.Patch(color='silver', label='Diff <= 0.1')
+ax.legend(handles=[gold, silver], fontsize=18, loc='lower right')
 
-# Make the plot
-h = 0.8
-fig, ax = plt.subplots(figsize=(12, 10))
+# plot
 bars = ax.barh(ind, diffs, h, align='center', color=colors, alpha=1)
 autolabelh(bars, diffs)
-lg = ax.legend(handles=[gold, silver], fontsize=18)
+
 # this next bit of madness is because i'm using both latex and string
 # formatting in the same line
 xlbl_str = '(B-V)$_{' + 'cloud' + '}$ - (B-V)$_{' + comparetype + '}$'
-xlbl = ax.set_xlabel(xlbl_str, fontsize=18)
-yt = ax.set_yticks(ind)
-ytl = ax.set_yticklabels(filters)
+ax.set_xlabel(xlbl_str, fontsize=18)
+ax.set_yticks(ind)
+ax.set_yticklabels(filters)
 plt.tick_params(axis='both', which='major', labelsize=16)
 titl = ax.set_title(
     'Difference in measured B-V index of cirrus clouds and the {} \n'
@@ -211,24 +231,47 @@ plt.savefig(saveloc + '/B-Vdiff_{}.png'.format(comparetype),
 
 # Plot B-V for cloud next to B-V for moon with error bars ----------------------
 h = 0.4
-comparecolor = 'gray' if comparetype=='moon' else 'yellow'
+comparecolor = 'black' if comparetype == 'moon' else 'orangered'
+compareErrColor = 'gray' if comparetype == 'moon' else 'orange'
 
-fig2, ax2 = plt.subplots(figsize=(20, 10))
-rects1 = ax2.barh(ind, cloudbv, h, color='cyan', align='center', alpha=0.7,
-                  xerr=clouderr, error_kw=dict(ecolor='black'))
-rects2 = ax2.barh(ind + h, comparebv, h, color=comparecolor, align='center',
-                  alpha=0.8, xerr=compareErr, error_kw=dict(ecolor='black'))
-lg2 = ax2.legend((rects1[0], rects2[0]), ('Clouds', comparetype), fontsize=18)
-xlbl2 = ax2.set_xlabel('B-V', fontsize=18)
-yt2 = ax2.set_yticks(ind + h/4)
-ytl2 = ax2.set_yticklabels(filters)
+fig2, ax2 = plt.subplots(figsize=(22, 10))
+
+# plot the actual B-V data points
+ax2.errorbar(ind, cloudbv, yerr=clouderr, fmt='o', color='blue', label='Clouds',
+             ms=7, marker='D')
+ax2.errorbar(ind, comparebv, yerr=compareErr, fmt='o', color=comparecolor,
+             label='{}'.format(comparetype), ms=7, marker='x')
+
+# print B-V values next to their markers and collect the locations
+cloudbv_pts = []
+comparebv_pts = []
+for i, v in enumerate(cloudbv):
+    cloudbv_pts.append([i, v])
+    ax2.text(i+0.1, v, str(round(v, 2)), color='blue', fontsize=12)
+for i, v in enumerate(comparebv):
+    comparebv_pts.append([i, v])
+    ax2.text(i-0.4, v, str(round(v, 2)), color=comparecolor, fontsize=12)
+
+# graph partially transparent bars representing the error
+for pt, err in zip(cloudbv_pts, clouderr):
+    ax2.plot([pt[0], pt[0]], [pt[1]-err, pt[1]+err], lw=10, color="skyblue",
+             solid_capstyle="butt", alpha=0.4)
+for pt, err in zip(comparebv_pts, compareErr):
+    ax2.plot([pt[0], pt[0]], [pt[1]-err, pt[1]+err], lw=10,
+             color=compareErrColor, solid_capstyle="butt", alpha=0.3)
+
+# ticks and titles etc
+ax2.legend(fontsize=18)
+ax2.set_xticks(ind)
+ax2.set_xticklabels(filters, rotation="45", ha='right')
+ax2.set_xlabel('Blue-visual Filter combinations', fontsize=20)
+ax2.set_ylabel('B-V index', fontsize=20)
 plt.tick_params(axis='both', which='major', labelsize=16)
 titl2 = ax2.set_title('B-V of clouds compared to {} \n'
                       'Clouds: {} {}, {}: {}'.format(comparetype, clouddate,
                                                      cloudset, comparetype,
-                                                     comparedate), fontsize=22)
-autolabelh(rects1, cloudbv)
-autolabelh(rects2, comparebv)
+                                                     comparedate), fontsize=24)
+
 plt.savefig(saveloc + '/cloudsvs{}.png'.format(comparetype),
             bbox_inches='tight')
 
